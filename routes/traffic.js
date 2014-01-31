33866c9d9f80415817b2b8124e219490d3883c51
Doron Segal <doron2402@gmail.com>
2014-01-30 21:44:41 -0800

    traffic works great

--- routes/traffic.js
+++ routes/traffic.js
@@ -11,12 +11,26 @@ exports.counter = function(req, res){
 
   var Model = require('../models/trafficModel');
 	
-  return new Model.TrafficModel({id: null, campignId: req.params.campignId, media: req.body.media }).save().then(function(model,err){
-  	if (err)
-  		console.log('Error : %s', err);
+	
+	if (req.params.campignId > 0){
 
-  	console.log(model);
-  	return res.json({ data: 'success' });	
-  });  
+	  return new Model.TrafficModel({
+	  	campignId: req.params.campignId,
+	  	prod: req.body.prod || 'NULL',
+	  	channel: req.body.channel || 'NULL',
+	  	media: req.body.media || 'NULL',
+	  	size: req.body.size || 'NULL',
+	  	reffer: req.body.reffer || 'NULL',
+	  	erate: req.body.erate || 'NULL',
+	  	dayOfWeek: new Date().getDay()
+	  	}).save().then(function(model,err){
+	  	if (err)
+	  		console.log('Error : %s', err);
+
+	  	console.log(model);
+	  	return res.json({ data: 'success' });	
+	  });  	
+	}
+  	console.log('here..');
 
 };
