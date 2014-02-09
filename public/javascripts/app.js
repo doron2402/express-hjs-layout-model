e66a11e6cff16eabaafcb1d93896a9bb86832f93
boazhoch <boazmier@gmail.com>
2014-02-09 23:59:55 +0200

    changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -254,9 +254,18 @@ var myApp = angular.module('myApp', ['ngRoute','ngCookies']);
             method: 'GET',
             url: '/faq/all'
         }).success(function(data, status, headers, config) {
-            console.log(data);
-            $scope.faq = data;
-            $scope.faq.tags = data.keywords;
+
+                /*
+
+                faq should keywords should be an array,
+                so it can be iterated, or an alternative would be
+                to take data.keywords split it at "," then iterate over it
+                ( some thoughts about to be consider )
+
+                 */
+
+                console.log(data);q
+                $scope.faq = data;
           }).
           error(function(data, status, headers, config) {
             console.log('Something Went wrong');
